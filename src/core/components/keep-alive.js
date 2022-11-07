a0f73c54050aec649a55c5541956c57b43334168
amandakelake <lguangcong@163.com>
2022-11-07 09:31:30 +0800

    feat: keep-alive 注释

--- src/core/components/keep-alive.js
+++ src/core/components/keep-alive.js
@@ -58,12 +58,17 @@ const patternTypes: Array<Function> = [String, RegExp, Array]
 
 export default {
   name: 'keep-alive',
+  // keep-alive不会生成真实dom的关键
+  // initLifecycle时，Vue在初始化生命周期的时候，为组件实例建立父子关系会根据abstract属性决定是否忽略某个组件。
+  // 这里设置了abstract: true，那Vue就会跳过该组件实例。
+  // 最后构建的组件树中就不会包含keep-alive组件，那么由组件树渲染成的DOM树自然也不会有keep-alive相关的节点了。
+
   abstract: true,
 
   props: {
-    include: patternTypes,
-    exclude: patternTypes,
-    max: [String, Number]
+    include: patternTypes, // 缓存白名单
+    exclude: patternTypes, // 缓存黑名单
+    max: [String, Number] // 缓存组件实例上线
   },
 
   methods: {
@@ -88,17 +93,19 @@ export default {
 
   created () {
     this.cache = Object.create(null)
-    this.keys = []
+    this.keys = [] // 缓存的虚拟dom的key
   },
 
   destroyed () {
     for (const key in this.cache) {
+      // 不是简单删除，还要执行组件的destroyed生命钩子
       pruneCacheEntry(this.cache, key, this.keys)
     }
   },
 
   mounted () {
     this.cacheVNode()
+    // 实时监听黑白名单的变动
     this.$watch('include', val => {
       pruneCache(this, name => matches(val, name))
     })
@@ -113,8 +120,10 @@ export default {
 
   render () {
     const slot = this.$slots.default
+    // 找到第一个子组件对象
     const vnode: VNode = getFirstComponentChild(slot)
     const componentOptions: ?VNodeComponentOptions = vnode && vnode.componentOptions
+    // 处理组件options，根据黑白名单决定是否需要缓存组件
     if (componentOptions) {
       // check pattern
       const name: ?string = getComponentName(componentOptions)
@@ -129,6 +138,7 @@ export default {
       }
 
       const { cache, keys } = this
+      // 生成缓存key, 根据组件id和tag
       const key: ?string = vnode.key == null
         // same constructor may get registered as different local components
         // so cid alone is not enough (#3269)
@@ -136,6 +146,7 @@ export default {
         : vnode.key
       if (cache[key]) {
         vnode.componentInstance = cache[key].componentInstance
+        // 先移除，再更新 -> 类似 LRU cache
         // make current key freshest
         remove(keys, key)
         keys.push(key)
@@ -144,9 +155,12 @@ export default {
         this.vnodeToCache = vnode
         this.keyToCache = key
       }
-
+      // 渲染和执行被包裹组件的钩子函数需要用到
+      // 初始化组件生命周期时，当vnode.componentInstance和keepAlive同时为truly值时，不再进入$mount过程，
+      // 那mounted之前的所有钩子函数（beforeCreate、created、mounted）都不再执行
       vnode.data.keepAlive = true
     }
+    // 返回组件vnode
     return vnode || (slot && slot[0])
   }
 }
